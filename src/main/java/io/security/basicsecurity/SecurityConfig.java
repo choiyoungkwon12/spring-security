8cbd547b804abc6860b8031f3f0617b2e5025669
choiyoungkwon12 <hpwh4457@naver.com>
2022-07-12 23:51:20 +0900

    form 방식을 이용한 security 로그인

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,35 @@
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest().authenticated();
+
+        http
+            .formLogin()
+            /*.loginPage("/loginPage")*/
+            .defaultSuccessUrl("/")
+            .failureUrl("/")
+            .usernameParameter("userId")
+            .passwordParameter("passwd")
+            .successHandler((request, response, authentication) -> {
+                System.out.println("authentication.getName() " + authentication.getName());
+                response.sendRedirect("/");
+            })
+            .failureHandler((request, response, exception) -> {
+                System.out.println(exception.getMessage());
+                response.sendRedirect("/login");
+            })
+            .permitAll();
+    }
+}
