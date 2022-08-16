c2cd59a9dcb7cba598bd3c05c1487d1e5325850e
choiyoungkwon12 <hpwh4457@naver.com>
2022-08-17 01:32:24 +0900

    스프링 시큐리티 실습 초기 설정

---
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -0,0 +1,9 @@
+package io.security.corespringsecurity.security.configs;
+
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
