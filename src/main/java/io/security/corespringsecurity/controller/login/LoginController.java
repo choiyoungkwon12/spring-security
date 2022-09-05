6785a42df49f95fd25fb62c4990cb65f58296b67
choiyoungkwon12 <hpwh4457@naver.com>
2022-09-06 01:08:03 +0900

    인증 후 권한이 없는 인가 예외 발생 시 핸들러 구현 - AccessDeniedHandler

--- src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,10 +1,8 @@
 package io.security.corespringsecurity.controller.login;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import io.security.corespringsecurity.domain.Account;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +13,21 @@ public class LoginController {
 
     @GetMapping(value = "/login")
     public String login(@RequestParam(value = "error", required = false) String error,
-                        @RequestParam(value = "exception", required = false) String exception, Model model) {
+        @RequestParam(value = "exception", required = false) String exception, Model model) {
 
         model.addAttribute("error", error);
         model.addAttribute("exception", exception);
         return "login";
     }
+
+    @GetMapping("/denied")
+    public String accessDenied(@RequestParam(value = "exception", required = false) String exception, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String accountName =  (String)authentication.getPrincipal();
+        model.addAttribute("username", accountName);
+        model.addAttribute("exception", exception);
+
+        return "user/login/denied";
+    }
+
 }
--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.security.corespringsecurity.security.configs;
 
+import io.security.corespringsecurity.security.handler.CustomAccessDeniedHandler;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
@@ -30,42 +32,52 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource,
         AuthenticationSuccessHandler customAuthenticationSuccessHandler,
         AuthenticationFailureHandler cusAuthenticationFailureHandler) {
-            this.authenticationProvider = authenticationProvider;
-            this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
-            this.authenticationDetailsSource = authenticationDetailsSource;
+        this.authenticationProvider = authenticationProvider;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
+        this.authenticationDetailsSource = authenticationDetailsSource;
 
         this.cusAuthenticationFailureHandler = cusAuthenticationFailureHandler;
     }
 
-        @Override
-        protected void configure (AuthenticationManagerBuilder auth) throws Exception {
-            auth.authenticationProvider(authenticationProvider);
-        }
-
-        @Override
-        protected void configure (final HttpSecurity http) throws Exception {
-            http
-                .authorizeRequests()
-                .antMatchers("/", "/users","/user/login/**","/login*").permitAll()
-                .antMatchers("/mypage").hasRole("USER")
-                .antMatchers("/messages").hasRole("MANAGER")
-                .antMatchers("/config").hasRole("ADMIN")
-                .anyRequest().authenticated()
-
-                .and()
-
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/login_proc")
-                .authenticationDetailsSource(authenticationDetailsSource)
-                .successHandler(customAuthenticationSuccessHandler)
-                .failureHandler(cusAuthenticationFailureHandler)
-                .defaultSuccessUrl("/")
-                .permitAll();
-        }
-
-        @Override
-        public void configure (WebSecurity web) throws Exception {
-            web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-        }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider);
     }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/", "/users", "/user/login/**", "/login*  ").permitAll()
+            .antMatchers("/mypage").hasRole("USER")
+            .antMatchers("/messages").hasRole("MANAGER")
+            .antMatchers("/config").hasRole("ADMIN")
+            .anyRequest().authenticated()
+
+            .and()
+
+            .formLogin()
+            .loginPage("/login")
+            .loginProcessingUrl("/login_proc")
+            .authenticationDetailsSource(authenticationDetailsSource)
+            .successHandler(customAuthenticationSuccessHandler)
+            .failureHandler(cusAuthenticationFailureHandler)
+            .defaultSuccessUrl("/")
+            .permitAll()
+            .and()
+            .exceptionHandling()
+            .accessDeniedHandler(accessDeniedHandler())
+        ;
+    }
+
+    private AccessDeniedHandler accessDeniedHandler() {
+        CustomAccessDeniedHandler accessDeniedHandler = new CustomAccessDeniedHandler();
+        accessDeniedHandler.setErrorPage("/denied");
+        return accessDeniedHandler;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
